40da19f4130015db5c7690665e32a258248cf76a
Maxwell Siah <maxwellsiah@vlogichk.onmicrosoft.com>
2024-04-08 12:33:36 +0800

    This file is a complete mess fix it future Maxwell

--- day21/main.go
+++ day21/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"runtime/pprof"
+	"strconv"
 )
 
+type Poslayer struct {
+	pos   PosWalk
+	layer int
+}
 type Pos struct {
 	x int
 	y int
@@ -16,12 +23,22 @@ type PosWalk struct {
 	steps int
 }
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 var field = [][]byte{}
 var visited = map[PosWalk]int{}
-var brothers = map[PosWalk]map[PosWalk]int{}
+var brothers = map[Pos]map[Poslayer]int{}
 
 func main() {
-	file, err := os.Open("../inputs/day21/testinput.txt")
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+	file, err := os.Open("../inputs/day21/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -35,30 +52,81 @@ func main() {
 		}
 		field = append(field, row)
 	}
-	startX, startY := 0, 0
+	//startX, startY := 0, 0
+	explore := [][]int{}
 	for y := range field {
 		for x := range field[y] {
 			if field[y][x] == 'S' {
 				field[y][x] = '.'
-				startX = x
-				startY = y
+				explore = append(explore, []int{x, y})
 			}
 		}
 	}
-	walk(10, startX, startY, 1)
+	//key := coordToKey(explore[0][0], explore[0][1])
+
+	//walk(196, explore[0][0], explore[0][1], 1)
+	steps := 65
+	visited2 := [10000][10000]int{}
+	answer := 0
+	output := map[string]int{}
+	for i := 0; i < len(explore); i++ {
+		moves := [][]int{{0, -1}, {0, 1}, {-1, 0}, {1, 0}}
+		node := explore[i]
+		taken := visited2[node[1]+5000][node[0]+5000]
+		for m := range moves {
+			x := node[0] + moves[m][0]
+			y := node[1] + moves[m][1]
+			if visited2[y+5000][x+5000] != 0 {
+				continue
+			}
+			tX, tY, _ := getTile(x, y)
+			if field[tY][tX] == '.' && (taken+1)%2 == 0 {
+				if output[coordToKey(x, y)] > 0 {
+					fmt.Println(x, y)
+				} else {
+					output[coordToKey(x, y)] = 1
+				}
+				answer += 1
+			}
+			visited2[y+5000][x+5000] = taken + 1
+			if taken < steps {
+				explore = append(explore, []int{x, y})
+			}
+		}
+	}
+
 	result := 0
 	for i := range visited {
 		if i.steps == 0 {
-			//fmt.Println(i)
+
 			result++
 		}
 	}
+	fmt.Println(result)
+	//fmt.Println(answer)
+
+	fmt.Println(predict([]int{3917, 34920, 96829, 189644}, 202301))
+
 	for i := range brothers {
-		if i.steps == 0 {
-			fmt.Println(i, brothers[i])
+		//fmt.Println(i, brothers[i])
+		layerAndSteps := map[int][]int{}
+		for b := range brothers[i] {
+			if layerAndSteps[b.layer] == nil {
+				layerAndSteps[b.layer] = []int{}
+			}
+			//if b.pos.steps == 0 {
+			layerAndSteps[b.layer] = append(layerAndSteps[b.layer], brothers[i][b])
+			//}
+			//fmt.Print(b.layer, " ", brothers[i][b], ",")
 		}
+		//fmt.Println(brothers[i])
+		//fmt.Println(layerAndSteps)
 	}
 	fmt.Println(result)
+}
+
+func coordToKey(x int, y int) string {
+	return strconv.Itoa(x) + "," + strconv.Itoa(y)
 }
 
 // walk all points reachable from x,y. if is garden plot store the x,y and if it is even or odd step
@@ -73,16 +141,16 @@ func walk(steps int, posX int, posY int, taken int) {
 		x := posX + move[0]
 		y := posY + move[1]
 		//in bounds
-		tX, tY := getTile(x, y)
+		tX, tY, layer := getTile(x, y)
 		if field[tY][tX] == '.' {
 			even := taken % 2
 			pW := PosWalk{Pos{x, y}, even}
-			if visited[pW] == 0 || visited[pW] > taken {
+			if visited[pW] == 0 || taken < visited[pW] {
 				visited[pW] = taken
-				if brothers[PosWalk{Pos{tX, tY}, even}] == nil {
-					brothers[PosWalk{Pos{tX, tY}, even}] = map[PosWalk]int{}
+				if brothers[Pos{tX, tY}] == nil {
+					brothers[Pos{tX, tY}] = map[Poslayer]int{}
 				}
-				brothers[PosWalk{Pos{tX, tY}, even}][pW] = taken
+				brothers[Pos{tX, tY}][Poslayer{pW, layer}] = taken
 				explore = append(explore, []int{x, y})
 			}
 		}
@@ -92,18 +160,77 @@ func walk(steps int, posX int, posY int, taken int) {
 	}
 }
 
+func walk2(steps int, posX int, posY int, taken int) {
+	if steps == 0 {
+		return
+	}
+	moves := [][]int{{0, -1}, {0, 1}, {-1, 0}, {1, 0}}
+	for m := range moves {
+		move := moves[m]
+		x := posX + move[0]
+		y := posY + move[1]
+		//in bounds
+		tX, tY, layer := getTile(x, y)
+		even := taken % 2
+		pW := PosWalk{Pos{x, y}, even}
+		if visited[pW] == 0 {
+			visited[pW] = taken
+			if brothers[Pos{tX, tY}] == nil {
+				brothers[Pos{tX, tY}] = map[Poslayer]int{}
+			}
+			brothers[Pos{tX, tY}][Poslayer{pW, layer}] = taken
+		}
+	}
+}
+
 // translate x y modulo if pos then mod if negative ????
-func getTile(x int, y int) (int, int) {
+func getTile(x int, y int) (int, int, int) {
 	tX, tY := 0, 0
 	if x > 0 {
 		tX = x % len(field[0])
 	} else if x < 0 {
-		tX = (len(field[0]) - 1) + x%len(field[0])
+		tX = (len(field[0])) + x%len(field[0])
+		if tX == len(field[0]) {
+			tX = 0
+		}
 	}
 	if y > 0 {
 		tY = y % len(field)
 	} else if y < 0 {
-		tY = (len(field) - 1) + y%len(field)
+		tY = (len(field)) + y%len(field)
+		if tY == len(field) {
+			tY = 0
+		}
 	}
-	return tX, tY
+
+	if x < 0 {
+		x = x*-1 + len(field[0]) - 1
+	}
+	if y < 0 {
+		y = y*-1 + len(field) - 1
+	}
+	return tX, tY, x/len(field[0]) + y/len(field)
+}
+
+func predict(sequence []int, repeat int) int {
+	sequences := [][]int{sequence}
+	current := sequences[0]
+	for current[0] != 0 {
+		nextSequence := []int{}
+		for i := 0; i < len(current)-1; i++ {
+			nextSequence = append(nextSequence, current[i+1]-current[i])
+		}
+		sequences = append(sequences, nextSequence)
+		current = nextSequence
+	}
+	for len(sequences[0]) < repeat {
+		s := len(sequences) - 1
+		carry := 0
+		for s >= 0 {
+			carry += sequences[s][len(sequences[s])-1]
+			sequences[s] = append(sequences[s], carry)
+			s -= 1
+		}
+	}
+	return sequences[0][len(sequences[0])-1]
 }
